3f8330df66dd0160195e1be49de8d40ff1ef1d91
Muhammad Hamza <grz4488684@gmail.com>
2026-03-28 10:00:28 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -21,8 +22,4 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
-});
-export default defineConfig({
-  base: '/Solo-Leveling/', // Replace 'repo-name' with your actual repository name
-  plugins: [react()],
 });
